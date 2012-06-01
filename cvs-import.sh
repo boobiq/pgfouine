e0c4f7941556de5a730b739e67b8a54711560b4e
Denis Orlikhin <denis@ostrovok.ru>
2012-06-01 16:18:06 +0400

    cvsimport fix for the new git

--- cvs-import.sh
+++ cvs-import.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ve
 git checkout cvs
-git cvsimport -v -d :pserver:anonymous@cvs.pgfoundry.org:/cvsroot/pgfouine pgfouine
+git cvsimport -o cvs -v -d :pserver:anonymous@cvs.pgfoundry.org:/cvsroot/pgfouine pgfouine
 set +v
 echo
 echo "#"
